682a7040704619d2e27f9cb4ba332e5bf954cac0
Vlad Pircioiu <vlad@futuredecoded.ro>
2019-01-26 20:08:52 +0200

    added predictions for titanic

--- src/titanic/titanic.py
+++ src/titanic/titanic.py
@@ -28,25 +28,53 @@ def save_df_object(df_raw):
 def split_vals(a,n): return a[:n].copy(), a[n:].copy()
 
 
-if __name__ == "__main__":
+def add_features(df_input):
+    df_input['family'] = df_input['SibSp'] + df_input['Parch'] + 1
+    df_input['isAlone'] = np.where(df_input['family'] > 1, False, True)
+    df_input['isChild'] = np.where(df_input['Age'] < 18, True, False)
+    df_input['title'] = df_input['Name'].apply(lambda x: x.split(',')[1]).apply(lambda x: x.split()[0])
+    return df_input
+
+
+def train_model():
     df_raw = get_raw_data()
+    df_raw = add_features(df_raw)
+    df_raw.drop(['Name', 'PassengerId', 'Ticket'], axis=1, inplace=True)
+    train_cats(df_raw)
+    df, y, nas = proc_df(df_raw, 'Survived')
+    n_trn = 700
+    X_train, X_valid = split_vals(df, n_trn)
+    y_train, y_valid = split_vals(y, n_trn)
 
-    print(df_raw.isnull().sum())
+    m = RandomForestRegressor(n_estimators=40,
+                              n_jobs=-1, oob_score=True)
+    m.fit(X_train, y_train)
 
-    print(df_raw['Parch'].value_counts())
-    print(df_raw.info())
+    # for feat, importance in zip(df.columns, m.feature_importances_):
+    #     print(
+    #     'feature: {f}, importance: {i}'.format(f=feat, i=importance))
+    #
+    # print(m.score(X_train, y_train))
+    # print(m.score(X_valid, y_valid))
+    # print(m.oob_score_)
+    #
+    # y_pred = m.predict(X_valid)
+    # print("Accuracy:", metrics.accuracy_score(y_pred.round(), y_valid))
 
-    print(df_raw.groupby('Sex')['Survived'].sum())
+    return m, df
 
-    df_raw['family'] = df_raw['SibSp'] + df_raw['Parch'] + 1
-    df_raw['isAlone'] = np.where(df_raw['family'] > 1, False, True)
-    df_raw['isChild'] = np.where(df_raw['Age'] < 18, True, False)
 
-    print(df_raw.groupby('family')['Survived'].sum())
-
-    df_raw['title'] = df_raw['Name'].apply(lambda x: x.split(',')[1]).apply(lambda x: x.split()[0])
-
-    print(df_raw.groupby('title')['Survived'].mean())
+if __name__ == "__main__":
+    # print(df_raw.isnull().sum())
+    #
+    # print(df_raw['Parch'].value_counts())
+    # print(df_raw.info())
+    #
+    # print(df_raw.groupby('Sex')['Survived'].sum())
+    #
+    # print(df_raw.groupby('family')['Survived'].sum())
+    #
+    # print(df_raw.groupby('title')['Survived'].mean())
     #print(df_raw['title'])
 
     # df_raw.plot(x='Embarked', y='Fare')
@@ -62,27 +90,27 @@ if __name__ == "__main__":
 
     # print(df_raw.count())
 
-    df_raw.drop(['Name', 'PassengerId', 'Ticket'], axis=1, inplace=True)
+    result = train_model()
+    m = result[0]
+    df_input = result[1]
+    print(df_input.info())
 
-    print(df_raw.head())
+    df_raw_test = pd.read_csv('../../data/titanic/test.csv', low_memory=False)
 
-    train_cats(df_raw)
-    df, y, nas = proc_df(df_raw, 'Survived')
-    n_trn = 700
-    X_train, X_valid = split_vals(df, n_trn)
-    y_train, y_valid = split_vals(y, n_trn)
+    df_raw_test['Fare'].fillna(df_raw_test['Fare'].mean(), inplace=True)
 
-    m = RandomForestRegressor(n_estimators=40,
-                               n_jobs=-1, oob_score=True)
-    m.fit(X_train, y_train)
+    print(df_raw_test.info())
 
-    for feat, importance in zip(df.columns, m.feature_importances_):
-        print(
-        'feature: {f}, importance: {i}'.format(f=feat, i=importance))
+    df_raw_test = add_features(df_raw_test)
 
-    print(m.score(X_train, y_train))
-    print(m.score(X_valid, y_valid))
-    print(m.oob_score_)
+    passengers_ids = df_raw_test['PassengerId']
 
-    y_pred = m.predict(X_valid)
-    print("Accuracy:", metrics.accuracy_score(y_pred.round(), y_valid))
+    df_raw_test.drop(['Name', 'PassengerId', 'Ticket'], axis=1, inplace=True)
+    train_cats(df_raw_test)
+    df, y, nas = proc_df(df_raw_test)
+
+    y_pred = m.predict(df).round().astype(int)
+
+    preds = pd.concat([pd.DataFrame(passengers_ids), pd.DataFrame(y_pred)], axis=1)
+
+    preds.to_csv('gender_submission.csv', sep=',', index=False)
